4c7a04e604e93752ead9a11983fdbd4802135816
nikalun <emptyfull@rambler.ru>
2016-09-09 22:22:01 +0300

    95% index

--- build/js/script.js
+++ build/js/script.js
@@ -20,11 +20,55 @@
         });
 
 
-       $('.swiper-slide').mouseover(function(){
+        $('.swiper-slide').mouseover(function(){
            swiper.stopAutoplay();
-       });
+        });
+
         $('.swiper-slide').mouseout(function(){
             swiper.startAutoplay();
+        });
+
+
+        ymaps.ready(function () {
+            var myMap = new ymaps.Map('map', {
+                    center: [52.89607457144356,40.506563499999984],
+                    zoom: 10
+                }, {
+                    searchControlProvider: 'yandex#search'
+                });
+
+
+            var myPlacemark2 = new ymaps.Placemark([52.89607457144356,40.506563499999984], {
+                address: "",
+                object: ""
+            }, {
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: '../img/point-map.png',
+                // Размеры метки.
+                iconImageSize: [30, 40],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-3, -42]
+            });
+
+            var myPlacemark = new ymaps.Placemark([52.705583571586466,41.39699149999999], {
+                address: "",
+                object: ""
+            }, {
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: '../img/point-map.png',
+                // Размеры метки.
+                iconImageSize: [30, 40],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-3, -42]
+            });
+
+            myMap.geoObjects.add(myPlacemark);
+            myMap.geoObjects.add(myPlacemark2);
+            myMap.behaviors.disable('scrollZoom');
         });
 
 
