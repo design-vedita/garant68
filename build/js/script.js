bcdc8b1cf8237b437a21132eedf3bffa7e472634
nikalun <emptyfull@rambler.ru>
2016-09-09 18:10:23 +0300

    index page 60%

--- build/js/script.js
+++ build/js/script.js
@@ -3,6 +3,30 @@
 
     $(function(){
 
+        $(".fancybox").fancybox({
+            openEffect	: 'none',
+            closeEffect	: 'none'
+        });
+
+        var swiper = new Swiper('.js-auto', {
+           // pagination: '.swiper-pagination',
+            direction: 'horizontal',
+            slidesPerView: 7.2,
+            paginationClickable: true,
+            spaceBetween: 7,
+            mousewheelControl: true,
+            autoplay: 2500,
+            autoplayDisableOnInteraction: true
+        });
+
+
+       $('.swiper-slide').mouseover(function(){
+           swiper.stopAutoplay();
+       });
+        $('.swiper-slide').mouseout(function(){
+            swiper.startAutoplay();
+        });
+
 
     });
 
